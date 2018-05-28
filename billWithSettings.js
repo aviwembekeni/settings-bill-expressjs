588e82ab9c5183f946bef329b67f6990b5e39d69
Aviwe Ngqukumba <aviwe3226@gmail.com>
2018-05-28 13:18:27 +0200

    initial commit

---
+++ billWithSettings.js
@@ -0,0 +1,135 @@
+module.exports = function(){
+
+  var bills = {call:0,
+               sms:0,
+               total: 0};
+
+ var settings = {callCostSetting: 2.75,
+                 smsCostSetting: 0.75,
+                 warningLevelSetting: 40,
+                 criticalLevelSetting: 75
+                 };
+
+ var color = "normal";
+
+ var billRecords = [];
+
+   var calculateRadioBillSett = function(billItemType){
+
+     let bill = {
+       billType : billItemType,
+       timeStamp : new Date()
+     }
+
+        if(billItemType == 'call'){
+          bills[billItemType] += settings.callCostSetting;
+          bill.cost = bills[billItemType];
+        }else if (billItemType == 'sms') {
+          bills[billItemType] += settings.smsCostSetting;
+          bill.cost = bills[billItemType];
+        }
+
+
+
+        bills['total'] = bills['sms'] + bills['call'];
+
+        billRecords.unshift(bill);
+
+        if (bills['total'] >= settings["criticalLevelSetting"]) {
+            color = "danger";
+        } else if(bills['total'] >= settings["warningLevelSetting"]){
+            color = "warning";
+        }
+
+    };
+
+    var updateCallCost = function(updatedCallCost){
+      if (updatedCallCost !== "") {
+         settings["callCostSetting"] = parseFloat(updatedCallCost);
+      }
+
+    }
+
+    var updateSmsCost = function(updatedSmsCost){
+      if (updatedSmsCost !== "") {
+          settings["smsCostSetting"] = parseFloat(updatedSmsCost);
+        }
+    }
+
+    var updateWarningLevelSetting = function(updatedWarningLevel){
+      if (updatedWarningLevel !== "") {
+        settings["warningLevelSetting"] = parseFloat(updatedWarningLevel);
+      }
+    }
+
+    var updateCriticalLevelSetting = function(updatedCriticalLevel){
+      if (updatedCriticalLevel !== "") {
+        settings["criticalLevelSetting"] = parseFloat(updatedCriticalLevel);
+      }
+    }
+
+     var checkCallCost = function(){
+         return  settings["callCostSetting"]
+     }
+
+     var checkSmsCost = function(){
+         return  settings["smsCostSetting"]
+     }
+
+     var checkWarningLevelSetting = function(){
+         return  settings["warningLevelSetting"]
+     }
+
+     var checkCriticalLevelSetting = function(){
+         return  settings["criticalLevelSetting"]
+     }
+
+    var checkRadioBillSett = function(billItemType){
+        return {
+          requestedBill : bills[billItemType],
+          sms : bills['sms'],
+          call : bills['call'],
+          total : bills['total']
+        }
+    }
+
+    var checkSettings = function(){
+      return settings;
+    }
+
+    var checkColor = function(){
+       return color;
+    }
+
+    var checkCallHistory = function(){
+      return billRecords.filter(bill => bill.billType === 'call');
+    }
+
+    var checkSmsHistory = function(){
+      return billRecords.filter(bill => bill.billType === 'sms');
+    }
+
+    var checkBillRecords = function(){
+      return billRecords
+    }
+
+    return {
+        calculate : calculateRadioBillSett,
+        check : checkRadioBillSett,
+        updateCall : updateCallCost,
+        checkCall : checkCallCost,
+        updateSms : updateSmsCost,
+        checkSms : checkSmsCost,
+        updateWarningLevel : updateWarningLevelSetting,
+        checkWarningLevel : checkWarningLevelSetting,
+        updateCriticalLevel : updateCriticalLevelSetting,
+        checkCriticalLevel : checkCriticalLevelSetting,
+        checkSettings,
+        checkColor,
+        checkCallHistory,
+        checkSmsHistory,
+        checkBillRecords
+
+    }
+
+}
---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs  = require('express-handlebars');
+var billWithSettings = require("./billWithSettings");
+var Moment = require("moment");
+
+const billSettings = billWithSettings();
+
+const app = express();
+
+let PORT = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs(
+  {defaultLayout: 'main',
+    helpers:{
+     "momentDate":function(){
+       return Moment(this.timeStamp).fromNow()
+     }
+   }
+ }));
+
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+app.use(express.static('public'))
+
+app.get("/", function(req, res){
+  var totals = billSettings.check();
+  var call_total = (totals.call).toFixed(2);
+  var sms_total = (totals.sms).toFixed(2);
+  var total = (totals.total).toFixed(2);
+  var color = billSettings.checkColor();
+  res.render("bill_settings", {call_total, sms_total, total, color });
+})
+
+app.post('/calculate', function(req, res){
+   var type = req.body.billType;
+   if(type !== undefined){
+      billSettings.calculate(type);
+   }
+
+   var totals = billSettings.check();
+   var call_total = (totals.call).toFixed(2);
+   var sms_total = (totals.sms).toFixed(2);
+   var total = (totals.total).toFixed(2);
+   var color = billSettings.checkColor();
+   res.render("bill_settings", {call_total, sms_total, total, color});
+
+    // note that data can be sent to the template
+    //res.render('bill_settings', {call_cost, sms_cost, warning_level, critical_level})
+});
+
+app.post('/update', function(req, res){
+   var call_cost = req.body.call_cost;
+   var sms_cost = req.body.sms_cost;
+   var warning_level = req.body.warning_level;
+   var critical_level = req.body.critical_level;
+
+  billSettings.updateCall(call_cost);
+  billSettings.updateSms(sms_cost);
+  billSettings.updateWarningLevel(warning_level);
+  billSettings.updateCriticalLevel(critical_level);
+
+    res.redirect('/');
+});
+
+app.get('/actions',function(req,res) {
+    var records = {bill: billSettings.checkBillRecords()}
+    res.render('Records', records);
+});
+
+app.get('/actions/:type',function(req,res) {
+   var billRecords = billSettings.check();
+
+  if(req.params.type=='call'){
+    let billRecord = {bill: billSettings.checkCallHistory()}
+    res.render('Records',billRecord);
+  }
+  else if(req.params.type=='sms'){
+   let billRecord = {bill: billSettings.checkSmsHistory()}
+   res.render('Records', billRecord);
+ }
+
+});
+
+app.listen(PORT, function(){
+  console.log('App starting on port', PORT)
+})
